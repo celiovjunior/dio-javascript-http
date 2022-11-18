04380d242db964fadbb0ecd202d12c8808c60f48
Célio Júnior <celio_cjr@hotmail.com>
2022-11-18 07:35:48 -0300

    fetch the data from the PokeAPI and showing the necessary data in the browser's console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,37 @@
-console.log("javascript connected")
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+function convertPokemonToHtml (pokemon) {
+  return `
+    <li class="pokemon">
+      <span class="number">#001</span>
+      <span class="name">${pokemon.name}</span>
+      <div>
+        <div class="details">
+          <ol class="types">
+            <li class="type">grass</li>
+            <li class="type">poison</li>
+          </ol>
+          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+        </div>
+      </div>
+    </li>
+    `
+}
+
+fetch(url)
+ .then(res => res.json())
+ .then(data => {
+   const pokemonsList = data.results
+   pokemonsList.forEach((item) => {
+    console.log(item.name)
+   })
+   debugger  
+  })
+ .catch(error => {
+  console.log(error)
+ })
+ .finally(() => {
+  console.log("Requisição concluída")
+ })
