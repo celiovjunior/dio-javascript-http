6ab636c072bdc18d9d1d8d92bb865661e46d75ad
Célio Júnior <celio_cjr@hotmail.com>
2022-11-19 08:06:00 -0300

    made the pagination feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,33 @@
-function convertPokemonToHtml (pokemon) {
-  return `
-    <li class="pokemon ${pokemon.type}">
-      <span class="number">#00${pokemon.number}</span>
-      <span class="name">${pokemon.name.toUpperCase()}</span>
-      <div>
-        <div class="details">
-          <ol class="types">
-            ${pokemon.types.map(type => `<li class="type ${pokemon.type}">${type}</li>`).join('')}
-          </ol>
-          <img src="${pokemon.photo}" alt="${pokemon.name}">
-        </div>
-      </div>
-    </li>
-  `
-}
 const pokemonList = document.getElementById('pokemon-list')
-pokeApi.getPokemons().then((pokemons = []) => {
-  pokemonList.innerHTML += pokemons.map(convertPokemonToHtml).join('')
+const loadMoreButton = document.getElementById('load-more-btn')
+const limit = 5
+let offset = 0
+
+function loadPokemonItems(offset, limit) {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newHtml = pokemons.map((pokemon) =>
+      `
+        <li class="pokemon ${pokemon.type}">
+          <span class="number">#00${pokemon.number}</span>
+          <span class="name">${pokemon.name.toUpperCase()}</span>
+          <div>
+            <div class="details">
+              <ol class="types">
+                ${pokemon.types.map(type => `<li class="type ${pokemon.type}">${type}</li>`).join('')}
+              </ol>
+              <img src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>
+          </div>
+        </li>
+      `
+    ).join('')
+    pokemonList.innerHTML += newHtml
+  })
+}
+
+loadPokemonItems(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+  offset += limit
+  loadPokemonItems(offset, limit)
 })
- .catch(error => {
-  console.log(error)
- })
- .finally(() => {
-  console.log("Requisição concluída")
- })
- 
