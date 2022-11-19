73e321db5a0ca297e1a714c7a4a9f5f5ff07b1f1
Célio Júnior <celio_cjr@hotmail.com>
2022-11-19 07:37:31 -0300

    adding new classes to set the right color for each type of pokemon and start to make the pagination feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,20 @@
-const pokemonList = document.getElementById('pokemon-list')
-function convertPokemonTypesToLi (pokemonTypes) {
-  return pokemonTypes.map((typeSlot) => {
-    return `<li class="type">${typeSlot.type.name}</li>`
-  })
-}
 function convertPokemonToHtml (pokemon) {
   return `
-    <li class="pokemon">
-      <span class="number">#00${pokemon.order}</span>
-      <span class="name">${pokemon.name}</span>
+    <li class="pokemon ${pokemon.type}">
+      <span class="number">#00${pokemon.number}</span>
+      <span class="name">${pokemon.name.toUpperCase()}</span>
       <div>
         <div class="details">
           <ol class="types">
-            ${convertPokemonTypesToLi(pokemon.types).join('')}
+            ${pokemon.types.map(type => `<li class="type ${pokemon.type}">${type}</li>`).join('')}
           </ol>
-          <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+          <img src="${pokemon.photo}" alt="${pokemon.name}">
         </div>
       </div>
     </li>
   `
 }
-
+const pokemonList = document.getElementById('pokemon-list')
 pokeApi.getPokemons().then((pokemons = []) => {
   pokemonList.innerHTML += pokemons.map(convertPokemonToHtml).join('')
 })
